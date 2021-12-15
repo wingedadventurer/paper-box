3d020eb005d21ed558862d0b0ff86cb9a70c8d9e
Karlo Koščal <wingedadventurer@gmail.com>
2021-12-15 15:10:06 +0100

    implement DataItem ScriptableObject; add Valve DataItem

---
+++ unity_project/Paper Box/Assets/ScriptableObjects/DataItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Data/Item", order = 1)]
+public class DataItem : ScriptableObject
+{
+    new public string name;
+}
