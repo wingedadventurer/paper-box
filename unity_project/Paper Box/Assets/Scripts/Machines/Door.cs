e63e22231c3aae44b961cb5a41373c1e5d18e2fe
Karlo Koščal <wingedadventurer@gmail.com>
2022-01-14 11:52:45 +0100

    redo Door machine; make it ask for keys

--- unity_project/Paper Box/Assets/Scripts/Machines/Door.cs
+++ unity_project/Paper Box/Assets/Scripts/Machines/Door.cs
@@ -6,20 +6,32 @@ public class Door : MonoBehaviour
 {
     public Animation anim;
     public AnimationClip acOpen;
-
-    private bool opened;
+    public Interactable interactableKey;
+    public DataItem requiredItem;
+    public GameObject goKey;
 
     private void Start()
     {
         anim.AddClip(acOpen, acOpen.name);
+
+        interactableKey.AddListener(Open);
+        goKey.SetActive(false);
     }
 
     public void Open()
     {
-        if (!opened)
+        if (Game.instance.dataItemSelected && Game.instance.dataItemSelected == requiredItem)
         {
-            opened = true;
+            interactableKey.gameObject.SetActive(false);
             anim.Play(acOpen.name);
+            Game.instance.OnInteractSuccess();
+            goKey.SetActive(true);
+            Invoke("ShowKey", 2);
         }
+    }
+
+    private void ShowKey()
+    {
+        goKey.SetActive(true);
     }
 }
