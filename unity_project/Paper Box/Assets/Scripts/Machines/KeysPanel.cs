9fd1229f5a3eac38d822fdb118506fd0f55d1c97
Karlo Koščal <wingedadventurer@gmail.com>
2022-02-17 10:54:12 +0100

    implement Keys Panel machine

--- unity_project/Paper Box/Assets/Scripts/Machines/KeysPanel.cs
+++ unity_project/Paper Box/Assets/Scripts/Machines/KeysPanel.cs
@@ -11,6 +11,54 @@ public class KeysPanel : MonoBehaviour
 
     private DataItem[] placed = new DataItem[192];
 
+    private void Start()
+    {
+        for (int i = 0; i < interactables.Length; i++)
+        {
+            int t = i;
+            interactables[i].AddListener( delegate { OnKeyPlace(t); } );
+        }
+
+        #region SETUP PATTERN
+        int a = 0;
+        pattern[18] = keyDatas[a++];
+        pattern[28] = keyDatas[a++];
+        pattern[10] = keyDatas[a++];
+        pattern[29] = keyDatas[a++];
+        pattern[16] = keyDatas[a++];
+
+        pattern[51] = keyDatas[a++];
+        pattern[59] = keyDatas[a++];
+        pattern[40] = keyDatas[a++];
+        pattern[35] = keyDatas[a++];
+        pattern[38] = keyDatas[a++];
+
+        pattern[76] = keyDatas[a++];
+        pattern[75] = keyDatas[a++];
+        pattern[66] = keyDatas[a++];
+        pattern[77] = keyDatas[a++];
+        pattern[68] = keyDatas[a++];
+
+        pattern[122] = keyDatas[a++];
+        pattern[101] = keyDatas[a++];
+        pattern[121] = keyDatas[a++];
+        pattern[111] = keyDatas[a++];
+        pattern[113] = keyDatas[a++];
+
+        pattern[151] = keyDatas[a++];
+        pattern[149] = keyDatas[a++];
+        pattern[148] = keyDatas[a++];
+        pattern[152] = keyDatas[a++];
+        pattern[129] = keyDatas[a++];
+
+        pattern[190] = keyDatas[a++];
+        pattern[174] = keyDatas[a++];
+        pattern[182] = keyDatas[a++];
+        pattern[169] = keyDatas[a++];
+        pattern[167] = keyDatas[a++];
+        #endregion
+    }
+
     public void OnKeyPlace(int index)
     {
         DataItem data = Inventory.instance.GetEquippedItem();
