f6acb88ff9c307d9787f271316fff7d41023f4db
Karlo Koščal <wingedadventurer@gmail.com>
2022-02-17 12:24:18 +0100

    add visible sequence to Teleporter

--- unity_project/Paper Box/Assets/Scripts/Machines/Teleport.cs
+++ unity_project/Paper Box/Assets/Scripts/Machines/Teleport.cs
@@ -21,32 +21,14 @@ public class Teleport : MonoBehaviour
         entered = new List<int>(6);
         for (int i = 0; i < 6; i++) { entered.Add(0); }
 
-        buttons[0].SetActive(true);
-        buttons[1].SetActive(false);
-        buttons[2].SetActive(false);
-        buttons[3].SetActive(false);
-        buttons[4].SetActive(false);
-        buttons[5].SetActive(false);
-
-        interactables[1].AddListener( delegate { OnButtonPlaced(1); } );
-        interactables[2].AddListener( delegate { OnButtonPlaced(2); } );
-        interactables[3].AddListener( delegate { OnButtonPlaced(3); } );
-        interactables[4].AddListener( delegate { OnButtonPlaced(4); } );
-        interactables[5].AddListener( delegate { OnButtonPlaced(5); } );
-
-        interactablesButtons[0].AddListener(delegate { OnButtonPressed(0); });
-        interactablesButtons[1].AddListener(delegate { OnButtonPressed(1); });
-        interactablesButtons[2].AddListener(delegate { OnButtonPressed(2); });
-        interactablesButtons[3].AddListener(delegate { OnButtonPressed(3); });
-        interactablesButtons[4].AddListener(delegate { OnButtonPressed(4); });
-        interactablesButtons[5].AddListener(delegate { OnButtonPressed(5); });
-
-        interactables[0].gameObject.SetActive(false);
-        interactables[1].gameObject.SetActive(true);
-        interactables[2].gameObject.SetActive(true);
-        interactables[3].gameObject.SetActive(true);
-        interactables[4].gameObject.SetActive(true);
-        interactables[5].gameObject.SetActive(true);
+        for (int i = 0; i < 6; i++)
+        {
+            int a = i;
+            buttons[i].SetActive(false);
+            interactables[i].AddListener(delegate { OnButtonPlaced(a); });
+            interactablesButtons[i].AddListener(delegate { OnButtonPressed(a); });
+            interactables[i].gameObject.SetActive(true);
+        }
     }
 
     private void OnButtonPlaced(int i)
